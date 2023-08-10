862d972c54b395d435b64c2c4de301be489f7cd6
misael282828 <misael282828@gmail.com>
2023-08-10 17:10:22 -0400

    configurando allure para mostrar reportes html

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,6 +2,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
     
+    //recordar la version que esta usando npx cypress@12.13.0 run --env allure=true
 
   })
 })
