b376fb74c7cf293ef2d23c41381c3ffd9f371a9c
misael282828 <misael282828@gmail.com>
2023-08-16 01:50:08 -0400

    TC - Inicio de sesion invalido

--- cypress/e2e/tests/autenticacion.cy.js
+++ cypress/e2e/tests/autenticacion.cy.js
@@ -2,7 +2,10 @@ import { commonPageData } from "../pages/common-page/common-page.data";
 import { commonPageMethods } from "../pages/common-page/common-page.methods";
 import { LoginData } from "../pages/login/login.data";
 import { LoginMethods } from "../pages/login/login.methods";
+import { singupMethods } from "../pages/singup/singup.methods";
 import { Logger } from "../util/logger";
+
+const existinUser= LoginData.validCredential.username
 
 describe(commonPageData.testSuites.autenticacion, ()=>{
   it("Inicio de sesion valido", ()=>{
@@ -16,17 +19,39 @@ describe(commonPageData.testSuites.autenticacion, ()=>{
 
     Logger.stepNumber(3)
     Logger.step("Completar los campos con informacion valida' ")
-    LoginMethods.insertUserName(LoginData.validCredential.username)
+    LoginMethods.insertUserName(existinUser)
     LoginMethods.insertPassoword(LoginData.validCredential.password)
 
     
     Logger.stepNumber(4)
     Logger.step("Hacer click en el 'Log in' para iniciar sesion")
     LoginMethods.clickOnLoginButton()
-    Logger.verification("Veridicar mensaje de alerta 'Sign up successful.' ")
-    commonPageMethods.verifySingedUser(LoginData.validCredential.username)
-
-
-
+    Logger.verification("Veridicar mensaje de alerta 'Welcome' ")
+    commonPageMethods.verifySingedUser(existinUser)
   })
+
+  it("Inicio de sesion invalido", ()=>{
+    Logger.stepNumber(1)
+    Logger.step("Navegar a la pagina de inicio")
+    commonPageMethods.navegateToDemoBlaze()
+
+    Logger.stepNumber(2)
+    Logger.step("Hacer click en el 'Log in' ")
+    commonPageMethods.clickOnloginOption()
+
+    Logger.stepNumber(3)
+    Logger.step("Completar los campos con informacion invalidas' ")
+    LoginMethods.insertUserName(existinUser)
+    LoginMethods.insertPassoword('invalidPass')
+
+    
+    Logger.stepNumber(4)
+    Logger.step("Hacer click en el 'Log in' para iniciar sesion")
+    LoginMethods.clickOnLoginButton()
+    Logger.verification("Veridicar mensaje de alerta 'Wrong password.' ")
+    singupMethods.verifyAlertMessege("Wrong password.")
+  })
+
+
+
 })
